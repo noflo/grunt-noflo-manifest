13312b1bda9dfd3a72b9215797be693c68df783b
Henri Bergius <henri.bergius@iki.fi>
2014-04-23 15:58:23 +0200

    Initial working version

---
+++ tasks/noflo_manifest.js
@@ -0,0 +1,158 @@
+/*
+ * grunt-noflo-manifest
+ * https://github.com/noflo/grunt-noflo-manifest
+ *
+ * Copyright (c) 2014 Henri Bergius
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var path = require('path');
+
+function updateGraph (platforms, id, localPath, type) {
+  Object.keys(platforms).forEach(function (platform) {
+    if (type !== 'all' && type !== platform) {
+      return;
+    }
+
+    if (!platforms[platform].graphs) {
+      platforms[platform].graphs = {};
+    }
+    platforms[platform].graphs[id] = localPath;
+  });
+}
+function updateComponent (platforms, id, localPath, type) {
+  Object.keys(platforms).forEach(function (platform) {
+    if (type !== 'all' && type !== platform) {
+      return;
+    }
+
+    if (!platforms[platform].components) {
+      platforms[platform].components = {};
+    }
+    platforms[platform].components[id] = localPath;
+  });
+}
+
+function parseId (source, filepath) {
+  var id = source.match(/@name ([A-Za-z0-9]+)/);
+  if (id) {
+    return id[1];
+  }
+  return path.basename(filepath, path.extname(filepath));
+}
+function parsePlatform (source) {
+  var runtimeType = source.match(/@runtime ([a-z\-]+)/);
+  if (runtimeType) {
+    return runtimeType[1];
+  }
+  return 'all';
+}
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('noflo_manifest', 'Grunt plugin for updating NoFlo package manifests', function() {
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      var platforms = {
+        'noflo-nodejs': {},
+        'noflo-browser': {}
+      };
+      var packageJson, componentJson;
+
+      // Load existing manifest data
+      if (grunt.file.exists(f.dest)) {
+        if (path.basename(f.dest) === 'package.json') {
+          packageJson = grunt.file.readJSON(f.dest);
+        }
+        if (path.basename(f.dest) === 'component.json') {
+          componentJson = grunt.file.readJSON(f.dest);
+        }
+      }
+
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).forEach(function (filepath) {
+        // Override manifest from source file
+        if (path.basename(filepath) === 'package.json') {
+          packageJson = grunt.file.readJSON(filepath);
+          return;
+        }
+        if (path.basename(filepath) === 'component.json') {
+          componentJson = grunt.file.readJSON(filepath);
+          return;
+        }
+        var ext = path.extname(filepath);
+        var localPath = path.relative(path.dirname(f.dest), filepath);
+        var graphData, componentData;
+        if (ext === '.json') {
+          // Graph
+          graphData = grunt.file.readJSON(filepath);
+          var id = graphData.properties.id || path.basename(filepath, '.json');
+          if (graphData.properties && graphData.properties.environment) {
+            updateGraph(platforms, id, localPath, graphData.properties.environment.type);
+            return;
+          }
+          updateGraph(platforms, id, localPath, 'all');
+          return;
+        }
+        if (ext === '.fbp') {
+          graphData = grunt.file.read(filepath);
+          updateGraph(platforms, parseId(graphData, filepath), localPath, parsePlatform(graphData));
+        }
+        if (ext === '.coffee' || ext === '.js' || ext === '.litcoffee') {
+          // Component
+          componentData = grunt.file.read(filepath);
+          updateGraph(platforms, parseId(componentData, filepath), localPath, parsePlatform(componentData));
+        }
+      });
+
+      var sourceJson;
+      var platform;
+      if (path.basename(f.dest) === 'package.json') {
+        // Node.js
+        if (!packageJson) {
+          grunt.fail.warn('Data for Node.js package file ' + f.dest + ' not found');
+          return;
+        }
+        sourceJson = packageJson;
+        platform = 'noflo-nodejs';
+      }
+      if (path.basename(f.dest) === 'component.json') {
+        if (!componentJson) {
+          grunt.fail.warn('Data for Component.io package file ' + f.dest + ' not found');
+          return;
+        }
+        sourceJson = componentJson;
+        platform = 'noflo-browser';
+      }
+
+      if (!sourceJson.noflo) {
+        sourceJson.noflo = {};
+      }
+      sourceJson.noflo.graphs = platforms[platform].graphs;
+      sourceJson.noflo.components = platforms[platform].components;
+      if (!sourceJson.noflo.graphs) {
+        delete sourceJson.noflo.graphs;
+      }
+      if (!sourceJson.noflo.components) {
+        delete sourceJson.noflo.components;
+      }
+
+      // Write the destination file.
+      grunt.file.write(f.dest, JSON.stringify(sourceJson, null, 2));
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" updated.');
+    });
+  });
+
+};
